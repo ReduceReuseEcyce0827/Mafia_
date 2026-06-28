20a61f1072347f35fcbc8466872e86faefbf557e
ReduceReuseEcyce0827 <ryumincookingdom@gmail.com>
2026-06-28 06:47:31 +0000

    2026-06-25-03-48

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,65 @@
 import streamlit as st
+import sqlite3 as sql
+class User:
+    def __init__(self, ID, PW, Name, Level, Exp):
+        self.ID = ID
+        self.PW = PW
+        self.Name = Name
+        self.Level = Level
+        self.Exp = Exp
+    def __str__(self):
+        return f"ID: {self.ID}, Password: {self.PW}, Name: {self.Name}, Level: {self.Level}, Exp: {self.Exp}"
+class Mission:
+    def __init__(self, ID, Text, Location, Description):
+        self.ID = ID
+        self.Text = Text
+        self.Location = Location
+        self.Description = Description
+    def __str__(self):
+        return f"ID: {self.ID}, Name: {self.Text}, Location: {self.Location}, Description: {self.Description}"
+class Role:
+    def __init__(self, ID, Name, is_neutral, Description):
+        self.ID = ID
+        self.Name = Name
+        self.is_neutral = is_neutral
+        self.Description = Description
+    def __str__(self):
+        return f"ID: {self.ID}, Name: {self.Name}, is_neutral: {self.is_neutral}, Description: {self.Description}"
+class Player:
+    def __init__(self, User: User, Role, NickName, Team, Votes, Missions, Status):
+        self.User = User
+        self.Role = Role
+        self.NickName = NickName
+        self.Team = Team
+        self.Votes = Votes
+        self.Missions = Missions
+        self.Status = Status
+    def __str__(self):
+        return f"User: [{self.User}], Role: {self.Role}, NickName: {self.NickName}, Team: {self.Team}, Votes: {self.Votes}, Missions: {self.Missions}, Status: {self.Status}"
+conn = sql.connect("mafia.db", timeout=float('inf'))
+cursor = conn.cursor()
+def Reset_Tables():
+    cursor.execute("DELETE FROM sessions")
+    cursor.commit()
+def Load_DB(Table: str):
+    cursor.execute(f"SELECT * FROM {Table}")
+    return cursor.fetchall()
+def Load_Users_Data():
+    return Load_DB("players")
+def User_Data_Conv_to_Class(User_Data):
+    (name, id, level, role_id, mission_id, status, vote_count, team, password, exp) = User_Data
+    data = [id, password, name, level, exp]
+    return User(data[0], data[1], data[2], data[3], data[4])
+def runApp():
+    st.title("D-0613 Laboratory Sinario Carbon Copy: Doppelganger Escape")
+    st.write("""
 
-st.title("D-0613 Laboratory Sinario Carbon Copy: Doppelganger Escape")
-st.write("""
-
-- 이벤트에 참여
-- 설정
-""")
+    #### 이벤트에 참여
+    #### 설정
+    """)
+Reset_Tables()
+Users = []
+for i in range(27):
+    Users.append(User_Data_Conv_to_Class(Load_Users_Data()[i]))
+print(Users[-4])
+runApp()
