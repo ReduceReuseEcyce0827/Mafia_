f9f0c86c21bdd3bf5fc90456415c75c0392cdb4a
ReduceReuseEcyce0827 <ryumincookingdom@gmail.com>
2026-06-28 06:32:29 +0000

    2026-06-25-03-34

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("D-0613 Laboratory Sinario Carbon Copy: Doppelganger Escape")
+st.write("""
+
+- 이벤트에 참여
+- 설정
+""")
