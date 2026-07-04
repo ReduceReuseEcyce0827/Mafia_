b4b2a454834737518ad923f8dea469d6cbc6c73b
ReduceReuseEcyce0827 <ryumincookingdom@gmail.com>
2026-07-04 06:23:13 +0000

    2026

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ def Change_Display(Where, PH, Users, Server613):
             st.title("로그인")
             ID = st.text_input("아이디")
             PW = st.text_input("비밀번호", type="password")
-            Login_B1 = st.button('로그인', 1, on_click=lambda: LoginB(PH, Server613, Users))
+            Login_B1 = st.button('로그인', 1, on_click=lambda: LoginB(PH, Server613, Users, ID, PW))
         return PH
     elif Where == "Admin":
         with PH.container():
@@ -123,7 +123,7 @@ def Change_Display(Where, PH, Users, Server613):
                 st.error("관리자 코드 인증 실패")
         return PH
     st.session_state.show_content = False
-def LoginB(place_holder, Server613, Users):
+def LoginB(place_holder, Server613, Users, ID, PW):
     if Server613 and ID in [user.ID for user in Users] and PW == [user.PW for user in Users if user.ID == ID][0]:
         st.success("로그인 성공")
         LoginSuccessed = True
