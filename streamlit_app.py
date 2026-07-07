53aeb13130eaab8280fe546344e07b82d3f98ce5
ReduceReuseEcyce0827 <ryumincookingdom@gmail.com>
2026-07-07 03:24:26 +0000

    2026

--- streamlit_app.py
+++ streamlit_app.py
@@ -299,7 +299,7 @@ def Change_Display(Where, Users):
                 st.session_state["team2C"][t2].send("SG".encode('utf-8'))
         if Buttons["Test"]:
             st.write(st.session_state["team1C"])
-            st.write(st.session_state["team1C"])
+            st.write(st.session_state["team2C"])
             for t1 in range(len(st.session_state["team1C"])):
                 st.session_state["team1C"][t1].send("테스트 메세지".encode('utf-8'))
             for t2 in range(len(st.session_state["team2C"])):
