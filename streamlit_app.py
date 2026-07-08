fe255fd43fb4faf9ff902a51e41b7dcffe8c7b18
ReduceReuseEcyce0827 <ryumincookingdom@gmail.com>
2026-07-08 05:51:46 +0000

    2026

--- streamlit_app.py
+++ streamlit_app.py
@@ -326,13 +326,6 @@ def Change_Display(Where, Users):
             client_socket.send("Hello!".encode('utf-8'))
         except socket.timeout:
             st.write("타임아웃!")
-        try:
-            client_socket, addr = st.session_state["ServerT2"][-1].accept()
-            st.session_state["team2C"].append(client_socket)
-            st.write(f"연결 수락됨: {addr}")
-            client_socket.send("Hello!".encode('utf-8'))
-        except socket.timeout:
-            st.write("타임아웃!")
         st.write(socket.gethostname())
         if Buttons["Start_T1"] and server:
             for t1 in range(len(st.session_state["team1C"])):
